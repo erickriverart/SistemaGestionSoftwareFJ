11087255fee6fd78c1654d0ec357e15520506067
Nicoll7 <nruizfo@unadvirtual.edu.co>
2026-05-09 11:13:46 -0500

    Refine error messages in exepciones.py
    Updated error messages for clarity and detail.

--- exepciones.py
+++ exepciones.py
@@ -1,13 +1,14 @@
-# Error cuando los datos del cliente son incorrectos
+#Error para datos inválidos del cliente e ingresa información 
+que no es correcta o está incompleta
 class ClienteInvalidoError(Exception):
     pass
 
-
-# Error cuando un servicio no está disponible
+# Error cuando un servicio no está disponible y el servicio solicitado no puede ser
+# ofrecido en ese momento 
 class ServicioNoDisponibleError(Exception):
     pass
 
-
-# Error relacionado con reservas
+# Error relacionado con reservas que controla errores
+# que ocurren durante el proceso de reserva
 class ReservaError(Exception):
     pass
